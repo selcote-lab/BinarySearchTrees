d46fca340e6001a680e1b5359e2ee081484d021c
zouhaire <zouhaire@idside.de>
2019-12-19 10:54:25 +0100

    impl delete for case 1 and 2

--- src/com/tonasolution/Tree.java
+++ src/com/tonasolution/Tree.java
@@ -30,22 +30,32 @@ public class Tree {
     }
 
     public void delete(int value) {
-
+        root = delete(root, value);
     }
 
-//    private TreeNode delete(TreeNode subtreeRoot, int value) {
-//
-//        if (subtreeRoot == null) {
-//            return subtreeRoot;
-//        }
-//
-//        if (value < subtreeRoot.getData()) {
-//            subtreeRoot.setLeft(delete(subtreeRoot.getLeft(), value));
-//        }
-//        else if (value > subtreeRoot.getData()) {
-//            subtreeRoot.setRight(delete(subtreeRoot.getRight(), value));
-//        }
-//    }
+    private TreeNode delete(TreeNode subtreeRoot, int value) {
+
+        if (subtreeRoot == null) {
+            return subtreeRoot;
+        }
+
+        if (value < subtreeRoot.getData()) {
+            subtreeRoot.setLeft(delete(subtreeRoot.getLeft(), value));
+        }
+        else if (value > subtreeRoot.getData()) {
+            subtreeRoot.setRight(delete(subtreeRoot.getRight(), value));
+        }
+        else {
+            // == Case 0 and 1 = node to delete has 0 or 1 child(ren)
+            if (subtreeRoot.getLeft() == null) {
+                return subtreeRoot.getRight();
+            }
+            else if (subtreeRoot.getRight() == null) {
+                return subtreeRoot.getLeft();
+            }
+        }
+        return subtreeRoot;
+    }
 
     public int max() {
         if (root == null) {
